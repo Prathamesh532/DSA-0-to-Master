01f788bf0364354b57eb21316e80c7bb8468f295
Prathamesh532 <prathameshkalekar0532@gmail.com>
2024-10-24 19:39:24 +0530

    BS-Day-13 1Q

--- 6_Binary_search/BS_on_Answers/12_find_median_2_sorted_arr.js
+++ 6_Binary_search/BS_on_Answers/12_find_median_2_sorted_arr.js
@@ -1,5 +1,5 @@
-let arr1 = [2, 4, 6];
-let arr2 = [1, 3];
+let arr1 = [1, 2];
+let arr2 = [3, 4];
 
 // brute force
 const findMedian = (arr1, arr2) => {
@@ -66,8 +66,8 @@ const findMedian_better = (arr1, arr2) => {
       cnt++;
       i++;
     } else {
-      if (cnt == index1) ele1 = arr2[i];
-      if (cnt == index2) ele2 = arr2[i];
+      if (cnt == index1) ele1 = arr2[j];
+      if (cnt == index2) ele2 = arr2[j];
       cnt++;
       j++;
     }
@@ -81,8 +81,8 @@ const findMedian_better = (arr1, arr2) => {
   }
 
   while (j < n2) {
-    if (cnt == index1) ele1 = arr2[i];
-    if (cnt == index2) ele2 = arr2[i];
+    if (cnt == index1) ele1 = arr2[j];
+    if (cnt == index2) ele2 = arr2[j];
     cnt++;
     j++;
   }
@@ -94,5 +94,34 @@ console.log("better Solution", findMedian_better(arr1, arr2));
 
 // using binary search
 const findMedian_usingBS = (arr1, arr2) => {
-    
+  let n1 = arr1.length;
+  let n2 = arr2.length;
+  if (n1 > n2) return findMedian_usingBS(arr2, arr1);
+
+  let n = n1 + n2;
+  let left = Math.floor((n1 + n2 + 1) / 2);
+
+  let low = 0;
+  let high = n1;
+
+  while (low <= high) {
+    let mid1 = Math.floor((low + high) / 2);
+    let mid2 = left - mid1;
+    let l1 = Number.MIN_SAFE_INTEGER;
+    let l2 = Number.MIN_SAFE_INTEGER;
+    let r1 = Number.MAX_SAFE_INTEGER;
+    let r2 = Number.MAX_SAFE_INTEGER;
+
+    if (mid1 < n1) r1 = arr1[mid1];
+    if (mid2 < n2) r2 = arr2[mid2];
+    if (mid1 - 1 >= 0) l1 = arr1[mid1 - 1];
+    if (mid2 - 1 >= 0) l2 = arr2[mid2 - 1];
+    if (l1 <= r2 && l2 <= r1) {
+      if (n % 2 == 1) return Math.max(l1, l2);
+      else return (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
+    } else if (l1 > r2) high = mid1 - 1;
+    else low = mid1 + 1;
+  }
+  return -1;
 };
+console.log("optimal Solution using BS", findMedian_usingBS(arr1, arr2));
