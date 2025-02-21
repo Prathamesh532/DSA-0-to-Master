6e9ae1c71ed5f00857a364360748e288087965af
Prathamesh532 <prathameshkalekar0532@gmail.com>
2025-02-21 18:56:37 +0530

    Bit-Manipulation Day-3 -- Basic programs lilke Add functions for bit manipulation: remove rightmost set bit, check power of 2, and count set bits, etc

--- 11_Bit_Manipulations/2_basic_question.js
+++ 11_Bit_Manipulations/2_basic_question.js
@@ -41,3 +41,52 @@ const toggleIthBit = (n, i) => {
 
 const toggleBit = toggleIthBit(5, 2);
 console.log("toggleBit", toggleBit);
+
+// rempve the rightmost set bit
+const removeRightMostSetBit = (n) => {
+  return n & (n - 1);
+};
+
+const removeRightMostBit = removeRightMostSetBit(5);
+console.log("removeRightMostBit", removeRightMostBit);
+
+// check the n is power of 2 or not
+const isPowOf2 = (n) => {
+  return (n & (n - 1)) === 0;
+};
+
+const powOf2 = isPowOf2(16);
+console.log("PowOf2", powOf2);
+
+//count the set (1) bit
+const countSetBits = (n) => {
+  let cnt = 0;
+
+  while (n > 1) {
+    // if (n % 2 === 1) cnt++;
+    // using bit operators --->
+    if (n & 1) cnt++; // OR cnt += n & 1
+
+    // n = Math.floor(n / 2);
+    // using bit operators --->
+    n = n >> 1;
+
+    if (n === 1) cnt++;
+  }
+
+  return cnt;
+};
+
+const countSetBits_ = (n) => {
+  let cnt = 0;
+
+  while (n !== 0) {
+    n = n & (n - 1);
+    cnt++;
+  }
+
+  return cnt;
+};
+
+const noOfSetBit = countSetBits_(5);
+console.log("noOfSetBit", noOfSetBit);
